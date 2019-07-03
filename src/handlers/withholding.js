26abf3e365503c4219fdec524f65d4dc30b08ef0
Alice1017 <genocidedragon@gmail.com>
2019-07-03 14:38:52 +0900

    MODIFIED: separate calculate module from handler

--- src/handlers/withholding.js
+++ src/handlers/withholding.js
@@ -1,23 +1,6 @@
+import calculateDeadline from "../modules/calculate";
 import moment from "moment";
 moment.locale("ja");
-
-const calculateDeadline = (year, month, date) => {
-    const deadline = moment().year(year).month(month-1).date(date);
-    const day = deadline.day(); // -> 曜日： 0 => 日曜, 6 => 土曜
-
-    // 曜日チェック
-    if (day === 0) {
-        // -> 1日ずらす
-        return calculateDeadline(year, month, (date+1));
-    }
-    else if (day === 6) {
-        // -> 2日ずらす
-        return calculateDeadline(year, month, (date+2));
-    }
-    else {
-        return deadline;
-    }
-};
 
 /**
  * The special case in Japanese: 納期の特例
@@ -35,7 +18,9 @@ const calculateDeadline = (year, month, date) => {
  * @param {Object} WithHoldingOptions
  */
 export default function withHolding(year, month, options={}) {
+
     const { useSpecialCase=false } = options;
+
     // -> January deadline is 20th if use special case
     const date = (useSpecialCase && month === 1) ? 20: 10;
     const deadline = calculateDeadline(year, month, date);
---
+++ src/modules/calculate.js
@@ -0,0 +1,19 @@
+import moment from "moment";
+
+export default function calculateDeadline (year, month, date) {
+    const deadline = moment().year(year).month(month-1).date(date);
+    const day = deadline.day(); // -> 曜日： 0 => 日曜, 6 => 土曜
+
+    // 曜日チェック
+    if (day === 0) {
+        // -> 1日ずらす
+        return calculateDeadline(year, month, (date+1));
+    }
+    else if (day === 6) {
+        // -> 2日ずらす
+        return calculateDeadline(year, month, (date+2));
+    }
+    else {
+        return deadline;
+    }
+};
